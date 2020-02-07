0ec19a407205b05e07a9fe009ef0db79c0a939da
atul-g <leoatul12@gmail.com>
2020-02-07 23:57:26 +0530

    added the loss and optimizers

--- gan_train.py
+++ gan_train.py
@@ -1,3 +1,4 @@
+import os
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, BatchNormalization, LeakyReLU, Conv2DTranspose, Reshape, Conv2D, Flatten, Dropout
@@ -33,3 +34,60 @@ discr_model=Sequential([
                         Dense(1)
                         ])
 discr_model.summary()
+
+
+#making losses:
+cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+
+def discriminator_loss(real_output, fake_output):
+    real_loss = cross_entropy(tf.ones_like(real_ouput), real_output)
+    fake_loss = cross_entropy(tf.ones_like(fake_output), fake_output)
+    total_loss=real_loss+fake_loss
+    return total_loss
+
+def generator_loss(fake_output):
+    return cross_entropy(tf.ones_like(fake_output), fake_output)
+
+generator_optimizer = tf.keras.optimizers.Adam(1e-4)
+discriminator_optimizer = tf.keras.optimizers.Adam(1e-4)
+
+
+###### saving checkpoints
+
+checkpoint_dir = './training_checkpoints'
+checkpoint_prefix=os.path.join(checkpoint_dir, "ckpt")
+checkpoint = tf.train.Checkpoint(generator_optimizer=generator_optimizer,
+                                 discriminator_optimizer=discriminator_optimizer,
+                                 generator=gen_model,
+                                 discriminator=discr_model)
+
+noise=tf.random.normal([1, 100])
+EPOCHS=50
+noise_dim=100
+num_examples_to_generate=16
+
+seed=tf.random.normal(['num_examples_to_generate', noise_dim])
+
+def train_step(images):
+    noise=tf.random.normal([BATCH_SIZE, noise_dim])
+    with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape"
+        generated_images = gen_model(noise, training=True)
+        
+        real_output=discr_model(images, training=True)
+        fake_output=discr_model(generated_images, training=True)
+        
+        discriminator_loss=discriminator_loss(real_output, fake_output)
+        gen_loss=generator_loss(fake_output)
+        
+    gradients_of_generator=gen_tape.gradient(gen_loss, gen_model.trainbable, variables)
+    disc_loss=discriminator_loss(real_output, fake_output)
+    gradients_of_discriminator = disc_tape.gradient(disc_loss, discr_model.trainable_variables)
+    
+    gradients_of_generator = gen_tape.gradient(gen_loss, gen_model.trainable_variables)
+    gradients_of_discriminator = disc_tape.gradient(disc_loss, discr_model.trainable_variables)
+
+    generator_optimizer.apply_gradients(zip(gradients_of_generator, gen_model.trainable_variables))
+    discriminator_optimizer.apply_gradients(zip(gradients_of_discriminator, discr_model.trainable_variables))
+
+
+
